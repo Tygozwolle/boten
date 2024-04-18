5c94dc109111aa3517da8cf06ab2e6796ade70e5
tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2024-04-18 17:16:04 +0200

    store secrets in code secure

---
+++ RoeiVerenigingLibary/Secrets.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoeiVerenigingLibary
+{
+    public  class Secrets
+    {
+        private IConfigurationRoot config = new ConfigurationBuilder().AddUserSecrets<Secrets>().Build();
+        public string DBAdress()
+        {
+            return config["DB:adress"];
+        }
+        public string DBUserName()
+        {
+            return config["DB:username"];
+        }
+        public string DBPassWord()
+        {
+            return config["DB:password"];
+        }
+    }
+}
