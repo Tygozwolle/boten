33e16498740edaf34f720e9db246964aacb44873
Tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2024-04-19 11:30:29 +0200

    commit voor simon

---
+++ DataAccessLibary/ConnectionString.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using RoeiVerenigingLibary;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibary
+{
+    public class ConnectionString
+    {
+       
+        public static string ConectionString()
+        {
+        IConfigurationRoot config = new ConfigurationBuilder().AddUserSecrets<ConnectionString>().Build();
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+       
+      
+      //  builder.DataSource = $"tcp:{config["DB:adress"]},22";
+        builder.UserID = config["DB:username"];
+        builder.Password = config["DB:password"];
+        builder.InitialCatalog = "boten_reservering";
+        
+           // builder.TransparentNetworkIPResolution = 
+            // builder.
+
+
+            //  string conection = $"Server = {config["DB:adress"]}:22; Database = boten_reservering; User Id = {config["DB:username"]}; Password = {config["DB:password"]};";
+            return $"Network Address=tcp:{config["DB:adress"]},22;" + builder.ConnectionString;
+            //return "Server=boten.vanolst.tech,22;Database=YourDatabaseName;User Id=boten;Password=Boten123456789;";
+        }
+    }
+}
--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace TestConsoleApp
+﻿using DataAccessLibary;
+using System.Data.SqlClient;
+
+namespace TestConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-        }
+            Console.WriteLine(ConnectionString.ConectionString());
+            using (SqlConnection connection = new SqlConnection(ConnectionString.ConectionString()))
+            {
+                connection.Open();
+                // Do work here; connection closed on following line.
+                String sql = "SELECT * FROM 'members'";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                        }
+                    }
+                }
+            }
+
+    } }
     }
-}
+
