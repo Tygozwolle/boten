d0e4b9e2733509753ffeca15b78253b4cf818fdb
YeetusDeletus2 <irispothof2005@gmail.com>
2024-04-19 13:09:54 +0200

    header is in seperate file

---
+++ RoeiVerenigingWPF/Frames/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace RoeiVerenigingWPF.Frames;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
