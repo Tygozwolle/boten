c33c24ce9b9972bf39e652e2c904f53e363cd598
Tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2024-04-22 14:36:46 +0200

    first test

---
+++ RoeiVerenigingUnitTests/MemberTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Moq;
+using RoeiVerenigingLibary;
+
+
+namespace RoeiVerenigingUnitTests
+{
+    public class MemberTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void InlogSuccesfull()
+        {
+            var member = new Member(1, "simon", "wil roeien", "simon@windesheim.nl", new List<string>());
+            var memberRepository = new Mock<IMemberRepository>();
+            memberRepository.Setup(x => x.Get(It.IsAny<string>(), It.IsAny<string>())).Returns(member);
+            var memberService = new MemberService(memberRepository.Object);
+            var result =  memberService.Login("simon@windesheim.nl", "Test1234");
+
+            Assert.AreEqual(result, member);
+        }
+    }
+}
