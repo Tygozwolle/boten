89bf0d71480394c86f2103649214228b5a675eeb
willeke huizing <38961397+willekeh@users.noreply.github.com>
2024-04-23 13:47:57 +0200

    timepicker

---
+++ RoeiVerenigingWPF/Frames/ReservationWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace RoeiVerenigingWPF.Frames
+{
+    /// <summary>
+    /// Interaction logic for ReservationWindow.xaml
+    /// </summary>
+    /// 
+    public partial class ReservationWindow : Window
+    {
+
+        public DateTime currentDateTime = DateTime.Now;
+
+        public ReservationWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+        }
+
+        public DateTime CurrentDateTime
+        {
+            get
+            {
+                return currentDateTime;
+            }
+            set
+            {
+                currentDateTime = value;
+            }
+        }
+    }
+}
