1be0f3eba37744c36a510db098c80da14c1621c0
Simon van den Berg <simonschrotenberg@gmail.com>
2024-04-23 14:35:31 +0200

    added loginPage

--- RoeiVerenigingWPF/Frames/MainWindow.xaml.cs
+++ RoeiVerenigingWPF/Frames/MainWindow.xaml.cs
@@ -1,13 +1,40 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
+using RoeiVerenigingLibary;
 using RoeiVerenigingWPF.Pages;
 
-namespace RoeiVerenigingWPF.Frames;
-
-public partial class MainWindow : Window
+namespace RoeiVerenigingWPF.Frames
 {
-    public MainWindow()
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        InitializeComponent();
-        MainContent.Navigate(new Login());
+        private Member? _loggedInMember;
+
+        public Member? LoggedInMember
+        {
+            get { return _loggedInMember; }
+            set
+            {
+                if (_loggedInMember != value)
+                {
+                    _loggedInMember = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainContent.Navigate(new Login(this));
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- RoeiVerenigingWPF/Pages/Login.xaml.cs
+++ RoeiVerenigingWPF/Pages/Login.xaml.cs
@@ -1,11 +1,27 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using DataAccessLibary;
+using RoeiVerenigingLibary;
+using RoeiVerenigingWPF.Frames;
 
 namespace RoeiVerenigingWPF.Pages;
 
 public partial class Login : Page
 {
-    public Login()
+    private MainWindow MainWindow;
+
+    public Login(MainWindow mainWindow)
     {
         InitializeComponent();
+        MainWindow = mainWindow;
+    }
+
+    public void LoginMember(object sender, RoutedEventArgs routedEventArgs)
+    {
+        string email = Email.Text;
+        string password = Password.Password;
+
+        MemberService service = new MemberService(new MemberRepository());
+        MainWindow.LoggedInMember = service.Login(email, password);
     }
 }
