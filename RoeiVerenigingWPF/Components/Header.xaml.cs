44bc0d5065a2c0fcafbc905c9466f3a2eca9b597
Tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2024-04-25 10:58:12 +0200

    removed command

--- RoeiVerenigingWPF/Components/Header.xaml.cs
+++ RoeiVerenigingWPF/Components/Header.xaml.cs
@@ -40,8 +40,6 @@ public partial class Header : UserControl
                     break;
                 }
         }
-        // MessageBox.Show(selectedContent);
-
     }
 
     private bool allowNavigation(string selectedContent)
