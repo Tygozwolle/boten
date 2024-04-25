1e19f1353ec0c9fa615e4aa753de5ed5361a5542
willeke huizing <38961397+willekeh@users.noreply.github.com>
2024-04-25 10:23:54 +0200

    first stuff

---
+++ DataAccessLibary/ReserveDB.cs
@@ -0,0 +1,31 @@
+﻿using MySqlConnector;
+
+namespace DataAccessLibary
+{
+    public class ReserveDB
+    {
+        private List<string> GetBoats()
+        {
+            var list = new List<string>();
+            using (MySqlConnection connection = new MySqlConnection(ConnectionString.GetString()))
+            {
+                connection.Open();
+                String sql = $"SELECT * FROM boats";
+                Console.WriteLine(sql);
+
+                using (MySqlCommand command = new MySqlCommand(sql, connection))
+                {
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            list.Add(reader.GetString(1));
+                        }
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ RoeiVerenigingLibary/Boats.cs
@@ -0,0 +1,18 @@
+﻿namespace RoeiVerenigingLibary
+{
+    public class Boats
+    {
+        public int Id { get; set; }
+        public int CaptainSeat { get; set; }
+        public int Seats { get; set; }
+        //public Url Image { get; set; }
+
+        public Boats(int id, int captainSeat, int Seats)
+        {
+            this.Id = id;
+            CaptainSeat = captainSeat;
+            this.Seats = Seats;
+
+        }
+    }
+}
