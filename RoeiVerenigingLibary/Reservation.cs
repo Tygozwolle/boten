1d20e4a9abd66597e0a258b58a2307f1aef8bb7c
Tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2024-05-06 10:33:01 +0200

    create reservation class

--- RoeiVerenigingLibary/Reservation.cs
+++ RoeiVerenigingLibary/Reservation.cs
@@ -2,5 +2,21 @@ namespace RoeiVerenigingLibary;
 
 public class Reservation
 {
-    
+    public Member Member { get; set; }
+    public DateTime CreationDate { get; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public int BoatID { get; set; }
+    public int Id {get;}
+
+    public Reservation(Member member, DateTime CreationDate, DateTime StartTime, DateTime EndTime, int BoatID, int ID)
+    {
+        this.Member = member;
+        this.CreationDate = CreationDate;
+        this.StartTime = StartTime;
+        this.EndTime = EndTime;
+        this.BoatID = BoatID;
+        this.Id = ID;
+    }
+
 }
