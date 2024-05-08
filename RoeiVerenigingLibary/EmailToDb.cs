62ed7e8525317931bee8494038ebf6784d0b6450
tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2024-05-08 12:00:17 +0200

    werkend

--- RoeiVerenigingLibary/EmailToDb.cs
+++ RoeiVerenigingLibary/EmailToDb.cs
@@ -5,6 +5,7 @@ using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using Aspose.Email;
+using Aspose.Email.Clients;
 using Aspose.Email.Clients.Imap;
 using Aspose.Email.Clients.Pop3;
 using Microsoft.Extensions.Configuration;
@@ -17,8 +18,11 @@ namespace RoeiVerenigingLibary
         {
             IConfigurationRoot config = new ConfigurationBuilder().AddUserSecrets<EmailToDb>().Build();
             ImapClient client = new ImapClient("imap.gmail.com", 993, config["Mail:username"], config["Mail:password"]);
-            client.SelectFolder(ImapFolderInfo.InBox);
-            ImapMessageInfoCollection messages = client.ListMessages();
+            client.SelectFolder(ImapFolderInfo.InBox);//Unread [Gmail]/Starred
+            ImapQueryBuilder builder = new ImapQueryBuilder();
+            builder.HasNoFlags(ImapMessageFlags.IsRead);
+            ImapMessageInfoCollection messages = client.ListMessages(builder.GetQuery());//
+            
             List<Aspose.Email.Attachment> attachments = new List<Aspose.Email.Attachment>();
             foreach (ImapMessageInfo messageInfo in messages)
             {
@@ -29,8 +33,14 @@ namespace RoeiVerenigingLibary
                     attachments.Add(attachment);
                     // Handle other attachment types similarly
                 }
+            //   client.RemoveMessageFlags(message.MessageId, ImapMessageFlags.Flagged );
+               //if (messageInfo.Flags.HasFlag(ImapMessageFlags.Flagged))
+               //{
+               //    client.RemoveMessageFlags(messageInfo.UniqueId, ImapMessageFlags.Flagged); //message.
+               //}
             }
             Console.WriteLine();
+            attachments.ToArray();
         }
 
     }
