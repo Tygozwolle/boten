ccf744dd5b88dd000dc5597bc4853c61b925e8d4
tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2024-05-09 12:05:49 +0200

    added lock

--- DataAccessLibary/ReservationRepository.cs
+++ DataAccessLibary/ReservationRepository.cs
@@ -48,7 +48,7 @@ public class ReservationRepository : IReservationRepository
 
     public List<Reservation> GetReservations()
     {
-        List<Reservation> reservations = new List<Reservation>();
+        List<Reservation> reservations;
 
         using (MySqlConnection connection = new MySqlConnection(ConnectionString.GetString()))
         {
@@ -60,6 +60,7 @@ public class ReservationRepository : IReservationRepository
                 using (MySqlDataReader reader = command.ExecuteReader())
                 {
                     List<Task> tasks = new List<Task>(reader.FieldCount);
+                    reservations = new List<Reservation>(reader.FieldCount);
 
                     while (reader.Read())
                     {
@@ -72,8 +73,12 @@ public class ReservationRepository : IReservationRepository
 
                         var task = new Task(() =>
                         {
-                            reservations.Add(new Reservation(MemberRepository.Get(memberId), creationDate,
+                            lock (reservations)
+                            {
+                                reservations.Add(new Reservation(MemberRepository.Get(memberId), creationDate,
                                 startTime, endTime, boatId, id));
+                            }
+                            
                         });
                         task.Start();
                         tasks.Add(task);
