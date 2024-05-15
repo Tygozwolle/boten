a48576e837ac40e2ca2a5fde49dfb5f499ea03fe
Tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2024-05-15 09:52:59 +0200

    setup repository

---
+++ DataAccessLibary/DamageRepository.cs
@@ -0,0 +1,14 @@
+﻿using RoeiVerenigingLibary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibary
+{
+    public class DamageRepository : IDamageRepository
+    {
+
+    }
+}
