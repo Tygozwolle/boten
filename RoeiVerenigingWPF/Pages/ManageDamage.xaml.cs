8876026559223d48926d03ea769f1482c5efa539
Simon van den Berg <simonschrotenberg@gmail.com>
2024-05-16 10:22:06 +0200

    added page for edit damage

--- RoeiVerenigingWPF/Pages/ManageDamage.xaml.cs
+++ RoeiVerenigingWPF/Pages/ManageDamage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using DataAccessLibary;
 using RoeiVerenigingLibary;
 using RoeiVerenigingWPF.Frames;
 
@@ -9,11 +11,27 @@ public partial class ManageDamage : Page
     private MainWindow _mainWindow { set; get; }
     public Damage Damage { set; get; }
 
+    private DamageService _service = new DamageService(new DamageRepository());
     public ManageDamage(MainWindow mw, Damage damage)
     {
         InitializeComponent();
         DataContext = this;
         _mainWindow = mw;
         Damage = damage;
+        Usable.IsChecked = damage.Usable;
+        Fixed.IsChecked = damage.BoatFixed;
+        Description.Text = damage.Description;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        bool _fixed = false;
+        bool _usable = false;
+        if (Fixed.IsChecked == true)
+            _fixed = true;
+        if (Usable.IsChecked == true)
+            _usable = true;
+        _service.Update(Damage.Id, _fixed, _usable, Description.Text);
+        _mainWindow.MainContent.Navigate(new ManageDamageOverview(_mainWindow));
     }
 }
