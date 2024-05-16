7188006b1b6150570596844a5efccae25868be0b
Tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2024-05-16 11:30:49 +0200

    description in boat

--- RoeiVerenigingLibary/Boat.cs
+++ RoeiVerenigingLibary/Boat.cs
@@ -7,13 +7,15 @@
         public int Seats { get; set; }
 
         public int Level { get; set; }
+        public string Description { get; set; }
 
-        public Boat(int id, Boolean captainSeat, int Seats, int level)
+        public Boat(int id, Boolean captainSeat, int Seats, int level, string description)
         {
             this.Id = id;
             CaptainSeat = captainSeat;
             this.Seats = Seats;
             Level = level;
+            this.Description = description;
         }
     }
 }
