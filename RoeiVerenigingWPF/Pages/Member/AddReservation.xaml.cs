4c33cff6c7263e43a3e634a789d62663cd25b4c0
YeetusDeletus2 <irispothof2005@gmail.com>
2024-05-24 12:36:30 +0200

    begin

--- RoeiVerenigingWPF/Pages/Member/AddReservation.xaml.cs
+++ RoeiVerenigingWPF/Pages/Member/AddReservation.xaml.cs
@@ -27,60 +27,60 @@ namespace RoeiVerenigingWPF.Pages
             DataContext = this;
         }
 
-        private void TimePicker_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            var textBox = FocusManager.GetFocusedElement(this) as TextBox;
-            if (textBox != null)
-            {
-            }
-        }
-
-        public void ConfirmButton(Object sender, RoutedEventArgs e)
-        {
-            //check if all fields are valid
-            DateTime? startTime = BeginTimePicker.Value;
-            DateTime? endTime = EndTimePicker.Value;
-            DateTime? selectedDate = calendar.SelectedDate;
-            if (!startTime.HasValue || !endTime.HasValue || !selectedDate.HasValue)
-            {
-                MessageBox.Show("Selecteer een datum en begin en eindtijd!");
-                return;
-            }
-
-            //add date and time together
-            DateTime startDateTime = selectedDate.Value.Date.Add(startTime.Value.TimeOfDay);
-            DateTime endDateTime = selectedDate.Value.Date.Add(endTime.Value.TimeOfDay);
-
-            try
-            {
-                if (_service.TimeChecker(startTime, endTime))
-                {
-                    _service.Create(_loggedInMember, boat.Id, startDateTime, endDateTime);
-                }
-            }
-            catch (InvalidTimeException invalidTimeException)
-            {
-                MessageBox.Show(invalidTimeException.Message);
-                return;
-            }
-            catch (MaxAmountOfReservationExceeded maxAmountOfReservationExceededException)
-            {
-                MessageBox.Show(maxAmountOfReservationExceededException.Message);
-                return;
-            }
-            catch (ReservationNotInDaylightException ex)
-            {
-                MessageBox.Show(ex.Message);
-                return;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                return;
-            }
-
-            MessageBox.Show("Reservering aangemaakt");
-            //todo: send to reservation overview
-        }
+        // private void TimePicker_TextChanged(object sender, TextChangedEventArgs e)
+        // {
+        //     var textBox = FocusManager.GetFocusedElement(this) as TextBox;
+        //     if (textBox != null)
+        //     {
+        //     }
+        // }
+        //
+        // public void ConfirmButton(Object sender, RoutedEventArgs e)
+        // {
+        //     //check if all fields are valid
+        //     DateTime? startTime = BeginTimePicker.Value;
+        //     DateTime? endTime = EndTimePicker.Value;
+        //     DateTime? selectedDate = calendar.SelectedDate;
+        //     if (!startTime.HasValue || !endTime.HasValue || !selectedDate.HasValue)
+        //     {
+        //         MessageBox.Show("Selecteer een datum en begin en eindtijd!");
+        //         return;
+        //     }
+        //
+        //     //add date and time together
+        //     DateTime startDateTime = selectedDate.Value.Date.Add(startTime.Value.TimeOfDay);
+        //     DateTime endDateTime = selectedDate.Value.Date.Add(endTime.Value.TimeOfDay);
+        //
+        //     try
+        //     {
+        //         if (_service.TimeChecker(startTime, endTime))
+        //         {
+        //             _service.Create(_loggedInMember, boat.Id, startDateTime, endDateTime);
+        //         }
+        //     }
+        //     catch (InvalidTimeException invalidTimeException)
+        //     {
+        //         MessageBox.Show(invalidTimeException.Message);
+        //         return;
+        //     }
+        //     catch (MaxAmountOfReservationExceeded maxAmountOfReservationExceededException)
+        //     {
+        //         MessageBox.Show(maxAmountOfReservationExceededException.Message);
+        //         return;
+        //     }
+        //     catch (ReservationNotInDaylightException ex)
+        //     {
+        //         MessageBox.Show(ex.Message);
+        //         return;
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         MessageBox.Show(ex.Message);
+        //         return;
+        //     }
+        //
+        //     MessageBox.Show("Reservering aangemaakt");
+        //     //todo: send to reservation overview
+        // }
     }
 }
