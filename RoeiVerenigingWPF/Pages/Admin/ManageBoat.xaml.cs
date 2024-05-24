e047612dcdf92d662bcbfe507fe3d78b436b7662
tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2024-05-24 12:26:24 +0200

    save

---
+++ RoeiVerenigingWPF/Pages/Admin/ManageBoat.xaml.cs
@@ -0,0 +1,41 @@
+﻿using DataAccessLibary;
+using RoeiVerenigingLibary;
+using System.Windows.Controls;
+
+namespace RoeiVerenigingWPF.Pages.Admin
+{
+    public partial class ManageBoat : Page
+    {
+        public ManageBoat()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BoatService service = new BoatService(new BoatRepository());
+            try
+            {
+                string firstName = FirstName.Text;
+                string infix = Infix.Text;
+                string lastName = LastName.Text;
+                string email = Email.Text;
+                string password = Password.Password;
+                Member createdMember = service.Create(_mainWindow.LoggedInMember, firstName, infix, lastName, email,
+                    password);
+                if (createdMember != null)
+                {
+                    MessageBox.Show(
+                        $"{createdMember.FirstName} {createdMember.Infix} {createdMember.LastName} is aangemaakt met lidnummer {createdMember.Id}");
+                }
+            }
+            catch (MemberAlreadyExistsException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (IncorrectRightsExeption ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
