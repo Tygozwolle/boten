d7fe21214e2cc1dbabf0de096792a8b020dc6508
tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2024-05-25 09:43:05 +0200

    made manage boat list

---
+++ RoeiVerenigingWPF/Pages/Admin/ManageBoatList.xaml.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLibrary;
+using RoeiVerenigingLibrary;
+using RoeiVerenigingWPF.Frames;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RoeiVerenigingWPF.Pages.Admin
+{
+    public partial class ManageBoatList : Page
+    {
+        private MainWindow _mainWindow;
+        private BoatService _service = new BoatService(new BoatRepository());
+        public List<Boat> Boats { get; set; }
+        public ManageBoatList(MainWindow mw)
+        {
+            DataContext = this;
+            Boats = _service.Getboats();
+            _mainWindow = mw;
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button)
+            {
+                Button casted = sender as Button;
+                object command = casted.CommandParameter;
+                int id = Int32.Parse(command.ToString());
+
+                _mainWindow.MainContent.Navigate(new ManageBoat(_mainWindow, _service.GetBoatById(id)));
+            }
+        }
+    }
+}
