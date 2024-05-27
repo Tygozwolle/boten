c9ffcd58425e5af1061c6e95b64743855216f158
tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2024-05-27 16:00:45 +0200

    made schade melding multi threaded

--- RoeiVerenigingLibrary/Model/Damage.cs
+++ RoeiVerenigingLibrary/Model/Damage.cs
@@ -13,6 +13,16 @@
             Usable = usable;
 
         }
+        public Damage(int id, Member member, Boat boat, string description, bool boatFixed, bool usable, DateTime reportTime)
+        {
+            Id = id;
+            Member = member;
+            Boat = boat;
+            Description = description;
+            BoatFixed = boatFixed;
+            Usable = usable;
+            ReportTime = reportTime;
+        }
 
         public Damage(int id, Member member, Boat boat, string description, bool boatFixed, bool usable,
             List<Stream> images)
@@ -25,12 +35,14 @@
             BoatFixed = boatFixed;
             Usable = usable;
         }
+        
         public int Id { get; set; }
         public Member Member { get; set; }
         public Boat Boat { get; set; }
         public string Description { get; set; }
         public bool BoatFixed { get; set; }
         public bool Usable { get; set; }
+        public DateTime ReportTime { get; set; }
         public List<Stream> Images { get; set; }
     }
 }
