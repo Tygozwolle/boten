47874cf9e1f6e854a00d27fceff0420356a8b344
Tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2024-05-28 14:32:00 +0200

    create event

---
+++ RoeiVerenigingLibrary/Model/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoeiVerenigingLibrary.Model
+{
+    public class Event
+    {
+        public List<Member> Participants { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int MaxParticipants { get; set; }
+        public List<Boat> Boats { get; set; }
+    }
+}
