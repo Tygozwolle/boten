cba0a8aeb86043118ca1ca00eab5fffa890e5a00
YeetusDeletus2 <irispothof2005@gmail.com>
2024-05-29 15:27:34 +0200

    prepare for merge

---
+++ RoeiVerenigingWPF/Pages/ViewStatistics.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace RoeiVerenigingWPF.Pages;
+
+public partial class ViewStatistics : Page
+{
+    public ViewStatistics()
+    {
+        InitializeComponent();
+    }
+}
