8d4972209fee174ac60bae8fe4e352eedce94d04
Simon van den Berg <simonschrotenberg@gmail.com>
2024-05-30 14:54:58 +0200

    added left collum

--- RoeiVerenigingWPF/Pages/Member/EventResult.xaml.cs
+++ RoeiVerenigingWPF/Pages/Member/EventResult.xaml.cs
@@ -9,8 +9,29 @@ namespace RoeiVerenigingWPF.Pages.Member;
 public partial class EventResult : Page
 {
     private MainWindow _MainWindow;
-    public Event EventResults;
+    public Event EventResults { get; set; }
     private IEventResultRepository _EventReportsRepository = new EventResultRepository();
+
+    public String Date
+    {
+        get { return EventResults.StartDate.Date.ToString("dd MMMM, yyyy"); }
+    }
+
+    public String StartTime
+    {
+        get { return EventResults.StartDate.ToString("HH:mm"); }
+    }
+
+    public String EndTime
+    {
+        get { return EventResults.EndDate.ToString("HH:mm"); }
+    }
+
+    public String ParticipantsCount
+    {
+        get { return EventResults.Participants.Count.ToString() + "/" + EventResults.MaxParticipants.ToString(); }
+    }
+
     public EventResult(MainWindow mw, Event _event)
     {
         DataContext = this;
