9e4b0c6d644c45fd3e10d3abf3eab0eb894a7dbf
sting-byte <hesp.rj@gmail.com>
2024-05-30 14:53:43 +0200

    added a graph library to show data in an easy way also made a new page for the trends

---
+++ RoeiVerenigingWPF/Pages/Admin/ViewTrends.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace RoeiVerenigingWPF.Pages.Admin;
+
+public partial class ViewTrends : Page
+{
+    
+    
+    public ViewTrends()
+    {
+        InitializeComponent();
+        var x = Enumerable.Range(0, 1001).Select(i => i / 10.0).ToArray();
+        var y = x.Select(v => Math.Abs(v) < 1e-10 ? 1 : Math.Sin(v) / v).ToArray();
+        bars.Plot(x); // x and y are IEnumerable<double>
+    }
+    
+}
