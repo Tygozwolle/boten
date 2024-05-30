8192a997269468e403cfd4d1f6fc9b11cbd74353
Simon van den Berg <simonschrotenberg@gmail.com>
2024-05-30 13:54:03 +0200

    a

--- RoeiVerenigingWPF/Pages/Member/EventResult.xaml.cs
+++ RoeiVerenigingWPF/Pages/Member/EventResult.xaml.cs
@@ -1,11 +1,22 @@
 ﻿using System.Windows.Controls;
+using DataAccessLibrary;
+using RoeiVerenigingLibrary;
+using RoeiVerenigingLibrary.Model;
+using RoeiVerenigingWPF.Frames;
 
 namespace RoeiVerenigingWPF.Pages.Member;
 
 public partial class EventResult : Page
 {
-    public EventResult()
+    private MainWindow _MainWindow;
+    public Event EventResults;
+    private IEventResultRepository _EventReportsRepository = new EventResultRepository();
+    public EventResult(MainWindow mw, Event _event)
     {
+        DataContext = this;
+        _MainWindow = mw;
+        EventResults = _event;
+        EventResults.AddParticipantsFromDatabase(_EventReportsRepository);
         InitializeComponent();
     }
 }
