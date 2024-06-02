dc1af2960ad3598adefb027ca639eab73436dadc
tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2024-06-02 19:54:41 +0200

    retry class

---
+++ DataAccessLibrary/Retry.cs
@@ -0,0 +1,34 @@
+﻿using MySqlConnector;
+
+namespace RoeiVerenigingLibrary
+{
+    public class Retry
+    {
+        public static T RetryMethod<T>(Func<T> method, int maxRetries = 3, int delay = 500)
+        {
+            for (int i = 0; i < maxRetries; i++)
+            {
+                try
+                {
+                    return method();
+                }
+                catch (MySqlException e)
+                {
+                    if (i == maxRetries - 1)
+                    {
+                        throw;
+                    }
+                }
+
+                if (delay != default)
+                {
+                    System.Threading.Thread.Sleep(delay);
+                }
+            }
+
+            return default;
+        }
+    }
+
+
+}
