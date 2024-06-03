32dd2d775ffe2213423778abaef69b3e3eaa8b71
Simon van den Berg <simonschrotenberg@gmail.com>
2024-06-03 10:47:06 +0200

    write tests

--- RoeiVerenigingUnitTests/EventResultTests.cs
+++ RoeiVerenigingUnitTests/EventResultTests.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using Moq;
 using RoeiVerenigingLibrary;
+using RoeiVerenigingLibrary.Exceptions;
 using RoeiVerenigingLibrary.Model;
 
 namespace RoeiVerenigingUnitTests;
@@ -8,16 +9,48 @@ namespace RoeiVerenigingUnitTests;
 public class EventResultTests
 {
     [Test]
-    public void SaveResultTime_CallsSaveTimeOnRepositoryWithCorrectParameters()
+    public void SaveResultTime_WithBeheerderRole_CallsSaveTimeOnRepository()
+    {
+        // Arrange
+        var mockRepository = new Mock<IEventResultRepository>();
+        Member member = new Member(1, "John", "", "Doe", "john.doe@example.com", new List<string> { "beheerder" }, 1);
+        var eventParticipant = new EventParticipant(member, 1, new TimeSpan(2, 0, 0), "Test Description");
+        mockRepository.Setup(r => r.SaveTime(It.IsAny<EventParticipant>()));
+
+        // Act
+        eventParticipant.saveResultTime(mockRepository.Object, member);
+
+        // Assert
+        mockRepository.Verify(r => r.SaveTime(eventParticipant), Times.Once);
+    }
+
+    [Test]
+    public void SaveResultTime_WithMateriaalCommissarisRole_CallsSaveTimeOnRepository()
+    {
+        // Arrange
+        var mockRepository = new Mock<IEventResultRepository>();
+        Member member = new Member(1, "John", "", "Doe", "john.doe@example.com",
+            new List<string> { "materiaal_commissaris" }, 1);
+        var eventParticipant = new EventParticipant(member, 1, new TimeSpan(2, 0, 0), "Test Description");
+        mockRepository.Setup(r => r.SaveTime(It.IsAny<EventParticipant>()));
+
+        // Act
+        eventParticipant.saveResultTime(mockRepository.Object, member);
+
+        // Assert
+        mockRepository.Verify(r => r.SaveTime(eventParticipant), Times.Once);
+    }
+
+    [Test]
+    public void SaveResultTime_WithoutRequiredRoles_ThrowsException()
     {
         // Arrange
         var mockRepository = new Mock<IEventResultRepository>();
         Member member = new Member(1, "John", "", "Doe", "john.doe@example.com", new List<string>(), 1);
         var eventParticipant = new EventParticipant(member, 1, new TimeSpan(2, 0, 0), "Test Description");
         mockRepository.Setup(r => r.SaveTime(It.IsAny<EventParticipant>()));
-        // Act
-        eventParticipant.saveResultTime(mockRepository.Object);
-        // Assert
-        mockRepository.Verify(r => r.SaveTime(eventParticipant), Times.Once);
+
+        // Act & Assert
+        Assert.Throws<IncorrectRightsException>(() => eventParticipant.saveResultTime(mockRepository.Object, member));
     }
 }
